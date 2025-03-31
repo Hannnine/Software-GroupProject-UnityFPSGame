013f08f8241550588e142280c59e27f8828ee266
宋晓 <whalexor@gmail.com>
2025-03-31 23:07:43 +0800

    Optimize
    Resolve the problem of the solution
    2k -> 1k

--- Assets/Script/Core/Map/UI/setscreen.cs
+++ Assets/Script/Core/Map/UI/setscreen.cs
@@ -2,17 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class setscreen : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        Screen.SetResolution(2560, 1440, true);
-    }
+public class setscreen : MonoBehaviour {
+	// Start is called before the first frame update
+	void Start() {
+		Screen.SetResolution(1960, 1080, true);
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// Update is called once per frame
+	void Update() {
+
+	}
 }
--- Assets/Script/Core/UI/ScreenAdapter.cs
+++ Assets/Script/Core/UI/ScreenAdapter.cs
@@ -1,67 +1,7 @@
 using UnityEngine;
 
-[RequireComponent(typeof(Camera))]
 public class ScreenAdapter : MonoBehaviour {
-	[Header("Display Settings")]
-	[SerializeField] Vector2Int defaultResolution = new Vector2Int(2560, 1440);
-	[SerializeField] Vector2Int minResolution = new Vector2Int(1280, 720);
-	[SerializeField] float targetAspect = 1.7778f;
-	[SerializeField] bool startFullscreen = true;
-
-	private Camera mainCamera;
-	private Resolution[] supportedResolutions;
-
 	void Start() {
-		mainCamera = GetComponent<Camera>();
-		InitializeDisplay();
-		UpdateAspectRatio();
-	}
-
-	void InitializeDisplay() {
-		supportedResolutions = Screen.resolutions;
-		Screen.SetResolution(
-				Mathf.Clamp(defaultResolution.x, minResolution.x, Screen.currentResolution.width),
-				Mathf.Clamp(defaultResolution.y, minResolution.y, Screen.currentResolution.height),
-				startFullscreen);
-	}
-
-	void Update() {
-		if (Input.GetKeyDown(KeyCode.F11))
-			ToggleFullscreen();
-		if (!Screen.fullScreen)
-			CheckWindowSize();
-	}
-
-	void CheckWindowSize() {
-		if (Screen.width < minResolution.x || Screen.height < minResolution.y) {
-			Screen.SetResolution(
-					Mathf.Max(Screen.width, minResolution.x),
-					Mathf.Max(Screen.height, minResolution.y),
-					false);
-		}
-	}
-
-	void ToggleFullscreen() {
-		Screen.fullScreen = !Screen.fullScreen;
-		if (!Screen.fullScreen)
-			UpdateAspectRatio();
-	}
-
-	void UpdateAspectRatio() {
-		float currentAspect = (float)Screen.width / Screen.height;
-		float scaleValue = currentAspect / targetAspect;
-
-		Rect cameraRect = mainCamera.rect;
-
-		if (scaleValue < 1.0f) {
-			cameraRect.height = scaleValue;
-			cameraRect.y = (1.0f - scaleValue) * 0.5f;
-		}
-		else {
-			cameraRect.width = 1.0f / scaleValue;
-			cameraRect.x = (1.0f - cameraRect.width) * 0.5f;
-		}
-
-		mainCamera.rect = cameraRect;
+		Screen.fullScreen = true;
 	}
 }

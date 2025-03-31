da9635f598a37118af6a24be07b24a9e082a3b56
宋晓 <whalexor@gmail.com>
2025-03-31 22:56:06 +0800

    Init commit

---
+++ Assets/Plugins/FPS/Scripts/Gameplay/HealthPickup.cs
@@ -0,0 +1,25 @@
+﻿using Unity.FPS.Game;
+using UnityEngine;
+
+namespace Unity.FPS.Gameplay
+{
+    public class HealthPickup : Pickup
+    {
+        [Header("Parameters")] [Tooltip("Amount of health to heal on pickup")]
+        public float HealAmount;
+        private void Update() {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        protected override void OnPicked(PlayerCharacterController player)
+        {
+            Health playerHealth = player.GetComponent<Health>();
+            if (playerHealth && playerHealth.CanPickup())
+            {
+                playerHealth.Heal(HealAmount);
+                PlayPickupFeedback();
+                Destroy(gameObject);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Network-GroupProject-UnityFPSGame
+# Software-GroupProject-UnityFPSGame
+
+# Abstract
+
+This is a 3D-FPS game based on `Unity`. The version of `Unity` is `2021.3.35f1`.
